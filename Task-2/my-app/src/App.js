90ad940056c763596781149daf7d464481712b12
Rutik Kulkarni <86470947+RutikKulkarni@users.noreply.github.com>
2022-01-20 20:07:10 +0530

    Add files via upload

---
+++ Task-2/my-app/src/App.js
@@ -0,0 +1,53 @@
+import Users from "./component";
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    // Set initial state
+    this.state = {
+      users_data: [],
+      loading: true
+    }
+    this.updateState = this.updateState.bind(this)
+  }
+
+  updateState() {
+    const link = "https://reqres.in/api/users?page=1";
+    fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+
+        this.setState({
+          users_data: users.data,
+          loading: false
+        })
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+  render() {
+    return (<>
+      <nav>
+        <div className="box">
+          <div className="row">
+            <div className="column1">
+              <h1><spam>U</spam>ser</h1>
+              <h1><button onClick={this.updateState}>Get Users</button></h1>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="box2">
+        <Users loading={this.state.loading} users={this.state.users_data} />
+      </div>
+    </>
+    )
+  }
+}
+
+export default App;
